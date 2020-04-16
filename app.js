c17068a0d6af996888308ac3e61dedae5f095cfb
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-04-16 13:44:22 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set the static folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index route
+app.get('/', (req, res) => {
+  res.render('index', {
+    stripePublishableKey: keys.stripePublishableKey,
+  });
+});
+
+// Charge route
+app.post('/charge', (req, res) => {
+  const amount = 2500;
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken,
+    })
+    .then((customer) =>
+      stripe.charges.create({
+        amount: amount,
+        description: 'Web Development Ebook',
+        currency: 'usd',
+        customer: customer.id,
+      })
+    )
+    .then((charge) => res.render('success'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
